c5b238c69b7f8101b210a14a3ecc8e0e07ba10a0
“olivcat” <184215778+olivcat@users.noreply.github.com>
2024-12-05 14:20:28 +0000

    Start of game - weapon selection added

--- script.js
+++ script.js
@@ -13,5 +13,21 @@ console.log("Hello, " + userName + "!");
 // console.log("Rock beats scissors, Scissors beats paper, Paper beats rock");
 //}
 
-let response = confirm("Would you like to read the guide?"); // leaves less room for user error in
-console.log("Rock beats scissors, Scissors beats paper, Paper beats rock");
+//let response = confirm("Would you like to read the guide?"); // leaves less room for user error in
+//console.log("Rock beats scissors, Scissors beats paper, Paper beats rock");
+//if (response === "yes" || response === "Yes")
+//console.log("Rock beats scissors, Scissors beats paper, Paper beats rock");
+
+let weapon = prompt(
+  "Choose your weapon:  1. Rock, 2. Paper, 3. Scissors. Type the number to make your choice"
+);
+
+if (weapon === "1") {
+  alert("You chose Rock!");
+} else if (weapon === "2") {
+  alert("You chose Paper!");
+} else if (weapon === "3") {
+  alert("You chose Scissors!");
+} else {
+  alert("Invalid choice. Please refresh and try again.");
+}
