481438d7524d2236678486e2c62df37d146fdb18
Sam Annetts <samannetts8@gmail.com>
2024-12-05 14:00:22 +0000

    getWinner function added

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ console.log("Hello, " + userName + "!");
 
 let response = confirm("Would you like to read the guide?"); // leaves less room for user error in
 console.log("Rock beats scissors, Scissors beats paper, Paper beats rock");
+
+
+
+
+function getWinner(player_choice, computer_choice) {
+
+  let payoff_table = [[0,-1,1],[1,0,-1],[-1,1,0]]
+
+  switch (player_choice) {
+    case "Rock":
+      player_choice = 0;
+      break;
+    case "Paper":
+      player_choice = 1;
+      break;
+    case "Scissors":
+      player_choice = 2;
+      break;
+}
+
+switch (computer_choice) {
+  case "Rock":
+    computer_choice = 0;
+    break;
+  case "Paper":
+    computer_choice = 1;
+    break;
+  case "Scissors":
+    computer_choice = 2;
+    break;
+}
+
+return payoff_table[player_choice][computer_choice];
+
+}
